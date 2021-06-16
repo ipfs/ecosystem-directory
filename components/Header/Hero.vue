6c9944332c3b2de30ea015b9842979b90566fa75
Nauras Jabari <nauras@agencyundone.com>
2021-06-16 19:23:56 -0400

    Chore: testing

--- components/Header/Hero.vue
+++ components/Header/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <section v-if="headerState" id="header-hero">
+  <section id="header-hero">
 
     <section
       v-if="pageData"
@@ -21,13 +21,13 @@
         <div class="col-9_sm-12">
           <div :class="{'headings-wrapper': true, 'results': (headerState === 'filters-applied')}">
 
-            <!-- <div v-show="(headerState === 'index-view')" class="index-heading">
+            <div v-if="(headerState === 'index-view')" class="index-heading">
               <h1>
                 {{ heading }}
               </h1>
             </div>
 
-            <div v-show="(headerState === 'filters-view')" class="filters-heading">
+            <div v-if="(headerState === 'filters-view')" class="filters-heading">
               <h1>
                 {{ heading }}
                 <span class="display-total">
@@ -36,7 +36,7 @@
               </h1>
             </div>
 
-            <div v-show="(headerState === 'filters-applied')" class="filters-heading">
+            <div v-if="(headerState === 'filters-applied')" class="filters-heading">
               <h1>
                 {{ heading }}
                 <span class="display-total">
@@ -45,21 +45,21 @@
               </h1>
             </div>
 
-            <div v-show="(headerState === 'index-view')" class="index-subheading">
+            <div v-if="(headerState === 'index-view')" class="index-subheading">
               {{ subheading }}
             </div>
 
-            <div v-show="(headerState === 'filters-view')" class="filters-subheading">
+            <div v-if="(headerState === 'filters-view')" class="filters-subheading">
               {{ subheading }}
             </div>
 
-            <div v-show="(headerState === 'filters-applied')" class="filters-subheading">
+            <div v-if="(headerState === 'filters-applied')" class="filters-subheading">
               <ul>
                 <li v-for="item in categories" :key="item.category">
                   {{ item.category }} <span class="tags">{{ item.tags }}</span>
                 </li>
               </ul>
-            </div> -->
+            </div>
 
           </div>
         </div>
@@ -119,19 +119,19 @@ export default {
     },
     headerState () {
       const route = this.$route
-      console.log(route)
-      return false
-      // if (route.name === 'index') {
-      //   if (route.query.filters === 'enabled') {
-      //     if (this.selectedFiltersCount) {
-      //       return 'filters-applied'
-      //     } else {
-      //       return 'filters-view'
-      //     }
-      //   }
-      //   return 'index-view'
-      // }
+      // console.log(route)
       // return false
+      if (route.name === 'index') {
+        if (route.query.filters === 'enabled') {
+          if (this.selectedFiltersCount) {
+            return 'filters-applied'
+          } else {
+            return 'filters-view'
+          }
+        }
+        return 'index-view'
+      }
+      return false
     },
     categories () {
       const filters = Taxonomy.categories
