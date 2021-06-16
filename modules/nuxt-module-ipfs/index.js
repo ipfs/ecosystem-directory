b27f2998df6c7977e8b8e656390ef95cf4628a0f
Nauras Jabari <nauras@agencyundone.com>
2021-06-16 17:14:32 -0400

    Chore: testing

--- modules/nuxt-module-ipfs/index.js
+++ modules/nuxt-module-ipfs/index.js
@@ -48,6 +48,7 @@ const parseRoute = (route) => {
 const addHooks = (instance) => {
   let staticAssetsOpts
   let parsed
+  let asyncScripts
 
   /*
     Grab the static asset path options to be applied in the render:routeContext
@@ -55,30 +56,15 @@ const addHooks = (instance) => {
   */
 
   instance.nuxt.hook('generate:before', (generator, generateOptions) => {
-    // console.log('============================ generator.nuxt.server.publicPath')
-    // // console.log(generator.nuxt.server.publicPath)
-    // generator.nuxt.server.publicPath = './'
-    // console.log('========================== generator.nuxt.renderer.publicPath')
-    // // console.log(generator.nuxt.renderer.publicPath)
-    // generator.nuxt.renderer.publicPath = './'
-    // console.log('===================== generator.options.generate.staticAssets')
-    // // console.log(generator.options.generate.staticAssets)
-    // generator.options.generate.staticAssets.base = generator.options.generate.staticAssets.base.slice(1)
-    // generator.options.generate.staticAssets.versionBase = generator.options.generate.staticAssets.versionBase.slice(1)
-    // console.log('==================================== generator.options.router')
-    // // console.log(generator.options.router)
-    // generator.options.router.base = './'
-    // console.log('===================================== generator.options.build')
-    // // console.log(generator.options.build)
-    // generator.options.build.publicPath = './'
-    // generator.options.build._publicPath = './'
-    // console.log('======================================= generator.options.app')
-    // // console.log(generator.options.app)
-    // generator.options.app.basePath = './'
-    // generator.options.app.assetsPath = './_nuxt'
-
-    // console.log(generateOptions)
     staticAssetsOpts = generateOptions.staticAssets
+  })
+
+  /*
+
+  */
+
+  instance.nuxt.hook('render:resourcesLoaded', (resources) => {
+    asyncScripts = resources.clientManifest.async
   })
 
   /*
@@ -87,21 +73,9 @@ const addHooks = (instance) => {
   */
 
   instance.nuxt.hook('vue-renderer:ssr:context', (ctx) => {
-    // console.log('B')
-    let path = ctx.nuxt.routePath
-    if (path.includes('/relativity')) {
-      path = path.replace('/relativity', '')
-      ctx.nuxt.routePath = path
-    }
-    parsed = parseRoute(path)
-    // // console.log(path)
-    // ctx.nuxt.config._app.basePath = parsed.replaceStatic
-    // ctx.nuxt.config._app.assetsPath = parsed.replaceSrc
-    // // Apply url replacements to generated javascript before it is serialized
+    parsed = parseRoute(ctx.nuxt.routePath)
+    // Apply url replacements to generated javascript before it is serialized
     ctx.staticAssetsBase = `${parsed.replaceSrc}${staticAssetsOpts.dir}/${staticAssetsOpts.version}`
-    // console.log(parsed)
-    // console.log(ctx.staticAssetsBase)
-    // console.log(ctx.nuxt.config)
   })
 
   /*
@@ -109,26 +83,56 @@ const addHooks = (instance) => {
   */
 
   instance.nuxt.hook('generate:page', async (payload) => {
-    // console.log('C')
-    // console.log(payload.route)
-    // console.log(payload)
     parsed = parseRoute(payload.route)
-    payload.html = payload.html.replace(/\/_nuxt\//gi, parsed.replaceSrc).replace(/\/relativity\//gi, parsed.replaceStatic)
-    const distPath = `${__dirname}/../../dist/_nuxt`
-    const filenames = await Fs.readdirSync(distPath)
-      .filter(filename => filename.includes('.js'))
-    console.log(filenames)
-    const len = filenames.length
-    for (let i = 0; i < len; i++) {
-      const filename = filenames[i]
-      let file = await Fs.readFileSync(`${distPath}/${filename}`) + ''
-      // console.log(file + '')
-      if (file.includes('"/_nuxt/"')) {
-        console.log(`INCLUDES: ${filename}`)
-        file = file.replace('"/_nuxt/"', '"_nuxt/"')
-        await Fs.writeFileSync(`${distPath}/${filename}`, file)
-      }
-    }
+    payload.html = payload.html
+      .replace(/\/_nuxt\//gi, parsed.replaceSrc)
+      .replace(/\/relativity\//gi, parsed.replaceStatic)
+    const script = `
+      <script>
+        var asyncScripts = ${JSON.stringify(asyncScripts)};
+        var len = asyncScripts.length;
+        var count = [];
+        var num = 0;
+        var interval = setInterval(function () {
+          if (num >= 50) {
+            clearInterval(interval)
+          }
+          for (var i = 0; i < len; i++) {
+            console.log(asyncScripts[i]);
+            var found = document.querySelector('script[src="' + asyncScripts[i] + '"]');
+            if (!found) {
+              console.log('NOT LOADING | ' + asyncScripts[i]);
+            }
+          }
+        }, 100);
+      </script>
+    `
+
+    // console.log(payload.html)
+
+    const split = payload.html.split('</body>')
+    const len = split.length
+    split.splice(1, len - 2, script)
+    payload.html = split.join('')
+    console.log(payload.html)
+
+
+    // const distPath = `${__dirname}/../../dist/_nuxt`
+    // const filenames = await Fs.readdirSync(distPath)
+    //   .filter(filename => filename.includes('.js'))
+    // // console.log(filenames)
+    // const len = filenames.length
+    // for (let i = 0; i < len; i++) {
+    //   const filename = filenames[i]
+    //   let file = await Fs.readFileSync(`${distPath}/${filename}`) + ''
+    //   // console.log(file + '')
+    //   if (file.includes('"/_nuxt/"')) {
+    //     // console.log(`INCLUDES: ${filename}`)
+    //     file = file.replace('"/_nuxt/"', '"_nuxt/"')
+    //     await Fs.writeFileSync(`${distPath}/${filename}`, file)
+    //   }
+    // }
+
     // console.log(payload.html.includes('relativity'))
     // if (payload.route.includes('brave')) {
     // }
