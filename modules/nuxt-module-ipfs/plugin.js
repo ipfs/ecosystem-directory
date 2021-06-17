cb7f0b8fe9bb7e6cadde40d5ca7411ce98cefe80
Nauras Jabari <nauras@agencyundone.com>
2021-06-17 12:20:29 -0400

    Chore: testing

--- modules/nuxt-module-ipfs/plugin.js
+++ modules/nuxt-module-ipfs/plugin.js
@@ -50,15 +50,15 @@ export default (context, inject) => {
     const ipfsPathRegExp = new RegExp('^(/(?:ipfs|ipns)/[^/]+)')
     const ipfsPathPrefix = (window.location.pathname.match(ipfsPathRegExp) || [])[1] || ''
 
-    console.log('__webpack_public_path__', __webpack_public_path__)
-    // console.log('context.app.router.history.base', context.app.router.history.base)
+    console.log(`__webpack_public_path__ | ${__webpack_public_path__}`)
+    console.log(`context.app.router.history.base | ${context.app.router.history.base}`)
 
     if (ipfsPathPrefix) {
       __webpack_public_path__ = ipfsPathPrefix + '/_nuxt/'
-      // context.app.router.history.base = ipfsPathPrefix
+      context.app.router.history.base = ipfsPathPrefix
     }
 
-    console.log('__webpack_public_path__', __webpack_public_path__)
-    // console.log('context.app.router.history.base', context.app.router.history.base)
+    console.log(`__webpack_public_path__ | ${__webpack_public_path__}`)
+    console.log(`context.app.router.history.base | ${context.app.router.history.base}`)
   }
 }
