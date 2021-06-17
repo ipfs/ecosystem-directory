26d7df0946e4e14658d97f13fcbdca89e508df61
Nauras Jabari <nauras@agencyundone.com>
2021-06-16 20:26:46 -0400

    Chore: testing

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,10 +85,10 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Proof of concepts for AU web frontend implementations in NuxtJS' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }
     ]
+    // link: [
+    //   { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }
+    // ]
   },
   // ////////////////////////////////////////// Customize the progress-bar color
   // ---------------------------------------------------------------------------
