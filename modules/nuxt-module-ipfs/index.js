9a836dbd1de83ad6329fbdbb0505376490406d84
Nauras Jabari <nauras@agencyundone.com>
2021-06-17 06:47:12 -0400

    Chore: testing

--- modules/nuxt-module-ipfs/index.js
+++ modules/nuxt-module-ipfs/index.js
@@ -82,22 +82,24 @@ const addHooks = (instance) => {
 
     const script = `
       <script>
-        if (typeof window !== 'undefined') {
-          const ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/
-          const ipfsPathPrefix = (window.location.pathname.match(ipfsPathRegExp) || [])[1] || ''
+        (function () {
+          if (typeof window !== 'undefined') {
+            const ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/
+            const ipfsPathPrefix = (window.location.pathname.match(ipfsPathRegExp) || [])[1] || ''
 
-          console.log('plugin __webpack_public_path__', __webpack_public_path__)
+            console.log('plugin __webpack_public_path__', __webpack_public_path__)
 
-          if (ipfsPathPrefix) {
-            __webpack_public_path__ = ipfsPathPrefix + '/_nuxt/'
+            if (ipfsPathPrefix) {
+              __webpack_public_path__ = ipfsPathPrefix + '/_nuxt/'
 
-            /*if (typeof window !== 'undefined') {
-              context.app.router.history.base = ipfsPathPrefix || window.location.host
-            }*/
+              /*if (typeof window !== 'undefined') {
+                context.app.router.history.base = ipfsPathPrefix || window.location.host
+              }*/
+            }
+
+            console.log('plugin __webpack_public_path__', __webpack_public_path__)
           }
-
-          console.log('plugin __webpack_public_path__', __webpack_public_path__)
-        }
+        }())
       </script>
     `
 
--- modules/nuxt-module-ipfs/plugin.js
+++ modules/nuxt-module-ipfs/plugin.js
@@ -41,14 +41,11 @@ export default (context, inject) => {
     const ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/
     const ipfsPathPrefix = (window.location.pathname.match(ipfsPathRegExp) || [])[1] || ''
 
-    console.log('plugin __webpack_public_path__', __webpack_public_path__)
+    // console.log('plugin __webpack_public_path__', __webpack_public_path__)
 
     if (ipfsPathPrefix) {
       // __webpack_public_path__ = ipfsPathPrefix + '/_nuxt/'
-
-      if (typeof window !== 'undefined') {
-        context.app.router.history.base = ipfsPathPrefix || window.location.host
-      }
+      context.app.router.history.base = ipfsPathPrefix || window.location.host
     }
 
     // console.log('plugin __webpack_public_path__', __webpack_public_path__)
