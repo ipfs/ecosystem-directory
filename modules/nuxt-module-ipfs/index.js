e6feab5cd4ce0668665b14d49ef0095df9f80424
Nauras Jabari <nauras@agencyundone.com>
2021-06-16 22:02:34 -0400

    Chore: testing

--- modules/nuxt-module-ipfs/index.js
+++ modules/nuxt-module-ipfs/index.js
@@ -89,10 +89,12 @@ const addHooks = (instance) => {
       if (file.includes('"/_nuxt/"') && !file.includes('return "/_nuxt/"')) {
         file = file.replace('"/_nuxt/"', `(function () {
           var pathname = window.location.pathname;
+          pathname = pathname.replace(/^\/+/, '');
+          pathname = pathname.replace(/\/+$/, '');
           var len = pathname.length;
-          console.log(pathname.slice(0, len));
-          if (pathname.charAt(0) === '/') { pathname = pathname.slice(1); }
-          if (pathname.charAt(len - 1) === '/') { pathname = pathname.slice(0, len); }
+          // console.log(pathname.slice(0, len));
+          // if (pathname.charAt(0) === '/') { pathname = pathname.slice(1); }
+          // if (pathname.charAt(len - 1) === '/') { pathname = pathname.slice(0, len); }
           console.log(pathname);
           var split = pathname.split("/");
           console.log(split);
