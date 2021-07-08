b94bd1388dcc45a7eeb4a0110c9bfc13f3a5dcfc
Ben Munoz <ben@agencyundone.com>
2021-07-08 13:25:59 -0400

    Chore: removed watch script

--- build.js
+++
@@ -1,9 +0,0 @@
-const { watch } = require('fs')
-const { exec } = require('child_process')
-
-watch('content/embeddable-view', (eventType, filename) => {
-  exec('node manifestor.js', (err, stdout, stderr) => {
-    if (err) return console.error(`Error: ${err}`);
-    console.info(`${filename} ${eventType}: Updated Manifestor`);
-  })
-})
